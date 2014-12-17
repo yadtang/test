fbf55fac57e119337d47a003c11fd9f62d041084
Lingqiusang130 <137590332@qq.com>
2014-12-18 00:40:55 +0800

    初始化

---
+++ test/src/test/Tester.java
@@ -0,0 +1,22 @@
+/**
+ * update:2014年12月18日 上午12:39:49 
+ */
+package test;
+
+/**
+ * @author lingqiusang
+ *
+ * @update:2014年12月18日 上午12:39:49
+ * 
+ */
+public class Tester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
